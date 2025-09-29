4b1548957f37d70e18ebb35e8a43f03964139b24
Tomofumishimokawa <tomofumi_shimokawa@mail.toyota.co.jp>
2025-09-29 10:22:55 +0900

    Update read_stl.py

--- read_stl.py
+++ read_stl.py
@@ -2,7 +2,6 @@ import numpy as np
 import torch
 from torch.utils.data import DataLoader, TensorDataset
 
-
 def read_stl_text(file_path):
     """
     Reads an ASCII STL file and extracts vertex coordinates and face indices.
